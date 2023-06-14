e62ea66320a2baa202f49a1a8043be61ad8cf8b1
Verner Codoceo <vacodoceo@uc.cl>
2023-06-14 02:18:35 -0400

    add sound

--- src/app/helpers/get-random-sound.ts
+++ src/app/helpers/get-random-sound.ts
@@ -1,6 +1,6 @@
 import { random, range } from "lodash-es";
 
-const sounds = range(8).map((i) => `sounds/sound${i + 1}.wav`);
+const sounds = range(9).map((i) => `sounds/sound${i + 1}.wav`);
 
 export const getRandomSound = () =>
   sounds.at(random(0, sounds.length - 1)) as string;
